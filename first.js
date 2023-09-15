64d4042fa6842ee8cf5c95cc4e54ba869dadbaeb
anyavoitovich <66640313+anyavoitovich@users.noreply.github.com>
2023-09-15 11:43:28 +0300

    Delete first.js

--- first.js
+++
@@ -1,9 +0,0 @@
-let numberOfFilms = prompt ('Сколько ильмов вы уже посмотрели?')
-let personalMovieDB = {
-    count: numberOfFilms,
-    movie: {}
-}
-let lastFilm1 = prompt ( 'Один из последних просмотренных фильмов? На сколько оцените его?')
-let answer1 = lastFilm1
-let lastFilm2 = prompt ( 'Один из последних просмотренных фильмов? На сколько оцените его?')
-let answer2 = lastFilm2
