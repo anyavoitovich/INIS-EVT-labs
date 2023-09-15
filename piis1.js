cbae6c2e8bcf818e1d1a5d75f52e803073c33786
anyavoitovich <66640313+anyavoitovich@users.noreply.github.com>
2023-09-15 11:45:09 +0300

    Add files via upload

---
+++ piis1.js
@@ -0,0 +1,9 @@
+let numberOfFilms = prompt ('Сколько ильмов вы уже посмотрели?')
+let personalMovieDB = {
+    count: numberOfFilms,
+    movie: {}
+}
+let lastFilm1 = prompt ( 'Один из последних просмотренных фильмов? На сколько оцените его?')
+let answer1 = lastFilm1
+let lastFilm2 = prompt ( 'Один из последних просмотренных фильмов? На сколько оцените его?')
+let answer2 = lastFilm2
