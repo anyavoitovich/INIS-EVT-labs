f77c15557173f0a3245b0610f22f379a92f23b32
anyavoitovich <anya.voitovich@icloud.com>
2023-10-18 18:56:50 +0300

    Update piis1.js

--- piis1.js
+++ piis1.js
@@ -1,9 +1,13 @@
 let numberOfFilms = prompt ('Сколько ильмов вы уже посмотрели?')
 let personalMovieDB = {
     count: numberOfFilms,
-    movie: {}
+    movie: []
 }
 let lastFilm1 = prompt ( 'Один из последних просмотренных фильмов? На сколько оцените его?')
 let answer1 = lastFilm1
-let lastFilm2 = prompt ( 'Один из последних просмотренных фильмов? На сколько оцените его?')
+let lastFilm2 = prompt ( ' На сколько оцените его?')
 let answer2 = lastFilm2
+let lastFilm3 = prompt ( 'Один из последних просмотренных фильмов? ')
+let answer3 = lastFilm3
+let lastFilm4 = prompt ( ' На сколько оцените его?')
+let answer4 = lastFilm4
