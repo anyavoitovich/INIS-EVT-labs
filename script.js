f37cb32fbbafe39c38c8a3545b068e8eca6efbdc
anyavoitovich <anya.voitovich@icloud.com>
2023-10-19 20:26:23 +0300

    finish 1.0

--- script.js
+++ script.js
@@ -24,21 +24,6 @@ function drawRectangle(event) {
     svg.innerHTML = `<rect x="${startX}" y="${startY}" width="${width}" height="${height}" stroke="black" fill="transparent"/>`;
 }
 
-function drawHeart(event) {
-    if (!isDrawing) return;
-
-    const width = event.clientX - startX;
-    const height = event.clientY - startY;
-
-    const heartPath = `
-        M ${startX} ${startY + height / 4}
-        Q ${startX + width / 2} ${startY - height / 2} ${startX + width} ${startY + height / 4}
-        Q ${startX + width / 2} ${startY + height} ${startX} ${startY + height / 4}
-        Z
-    `;
-
-    svg.innerHTML = `<path d="${heartPath}" stroke="black" fill="transparent"/>`;
-}
 
 function drawTriangle(event) {
     if (!isDrawing) return;
@@ -55,27 +40,6 @@ function drawTriangle(event) {
     svg.innerHTML = `<path d="${trianglePath}" stroke="black" fill="transparent"/>`;
 }
 
-
-
-function drawStar(event) {
-    if (!isDrawing) return;
-
-    const size = Math.min(event.clientX - startX, event.clientY - startY);
-    const angleOff = -Math.PI / 2;
-    const angleStep = Math.PI / 5;
-    let starPath = `M ${startX + Math.cos(angleOff) * size / 2} ${startY + Math.sin(angleOff) * size / 2}`;
-
-    for (let i = 1; i <= 5; i++) {
-        const angle = angleOff + i * 2 * angleStep;
-        const x = startX + Math.cos(angle) * size * (i % 2 === 0 ? 1 : 0.5);
-        const y = startY + Math.sin(angle) * size * (i % 2 === 0 ? 1 : 0.5);
-        starPath += ` L ${x} ${y}`;
-    }
-
-    starPath += ' Z';
-
-    svg.innerHTML = `<path d="${starPath}" stroke="black" fill="transparent"/>`;
-}
 
 
 function stopDrawing() {
