8369e562a4d5ac7b717089014c2fbbb239da5621
anyavoitovich <anya.voitovich@icloud.com>
2023-10-19 20:10:45 +0300

    new figures

--- script.js
+++ script.js
@@ -22,6 +22,51 @@ function draw(event) {
         svg.innerHTML = `<rect x="${startX}" y="${startY}" width="${width}" height="${height}" stroke="black" fill="transparent"/>`;
     }
 }
+function drawHeart(event) {
+    if (!isDrawing) return;
+
+    const width = event.clientX - startX;
+    const height = event.clientY - startY;
+    const heartPath = `
+        M ${startX} ${startY + height / 2}
+        Q ${startX} ${startY} ${startX + width / 2} ${startY}
+        Q ${startX + width} ${startY} ${startX + width} ${startY + height / 2}
+        L ${startX + width / 2} ${startY + height}
+        Z
+    `;
+
+    svg.innerHTML = `<path d="${heartPath}" stroke="black" fill="transparent"/>`;
+}
+
+function drawTriangle(event) {
+    if (!isDrawing) return;
+
+    const width = event.clientX - startX;
+    const height = event.clientY - startY;
+    const trianglePath = `
+        M ${startX} ${startY}
+        L ${startX + width} ${startY}
+        L ${startX + width / 2} ${startY + height}
+        Z
+    `;
+
+    svg.innerHTML = `<path d="${trianglePath}" stroke="black" fill="transparent"/>`;
+}
+
+function drawStar(event) {
+    if (!isDrawing) return;
+
+    const size = Math.min(event.clientX - startX, event.clientY - startY);
+    const starPath = `
+        M ${startX} ${startY - size / 2}
+        L ${startX + size / 5} ${startY + size / 5}
+        L ${startX} ${startY + size / 2}
+        L ${startX - size / 5} ${startY + size / 5}
+        Z
+    `;
+
+    svg.innerHTML = `<path d="${starPath}" stroke="black" fill="transparent"/>`;
+}
 
 function stopDrawing() {
     isDrawing = false;
