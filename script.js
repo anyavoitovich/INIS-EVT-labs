23246fe808cea8b1d261c96620117f3fa30e7d1f
anyavoitovich <anya.voitovich@icloud.com>
2024-02-06 13:11:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ function fall() {
   if (position <= 0) {
     // Учитываем вес мяча и коэффициент упругости при отскоке
     velocity *= -restitutionCoefficient / weight; 
+    velocity -= g * 0.01; // Ускорение при отскоке
 
     // Увеличиваем счетчик отскоков
     bounces++;
